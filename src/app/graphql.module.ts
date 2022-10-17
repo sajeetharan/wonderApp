e289af1a1b6863bbc42c1b867d8a85372f52e1d5
Sajeetharan <sajeetharan@outlook.com>
2022-10-17 22:22:39 +0530

    Update graphql.module.ts

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -12,7 +12,7 @@ const defaultOptions: DefaultOptions = {
     errorPolicy: 'all',
   },
 }
-const uri = 'https://dab-cosmos-demo.azurewebsites.net/graphql/'; // <-- add the URL of the DAB
+const uri = 'https://zealous-tree-0b6736615.azurestaticappsbeta.net/data-api/graphql'; // <-- add the URL of the DAB
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
